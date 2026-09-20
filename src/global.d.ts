e383ede442e80767b196159b87855fdd6d6369ea
tonitarung099-creator <tonitarung099@gmail.com>
2026-09-21 00:06:48 +0700

    feat: type account registry bridge

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,9 +1,26 @@
 export {};
+
+type StoredProfile = {
+  slot: number;
+  label: string;
+  chromeProfileName: string;
+  enabled: boolean;
+};
+
+type ChromeProfileCandidate = {
+  directory: string;
+  name: string;
+};
+
 declare global {
   interface Window {
     voiceAgent?: {
       getRuntimeState: () => Promise<{maxGoogleProfiles:number;maxGeminiApiKeys:number;version:string}>;
       getDownloadWindow: () => Promise<{open:boolean;localTime:string;label:string;nextLabel:string}>;
+      listProfiles: () => Promise<StoredProfile[]>;
+      discoverChromeProfiles: () => Promise<ChromeProfileCandidate[]>;
+      saveProfile: (profile: StoredProfile) => Promise<StoredProfile[]>;
+      removeProfile: (slot: number) => Promise<StoredProfile[]>;
     };
   }
 }
